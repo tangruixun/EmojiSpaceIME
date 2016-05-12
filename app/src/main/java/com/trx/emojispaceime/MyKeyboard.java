1f61b94217457260c3001418490b13edf77c48e5
tangruixun <tangruixun@gmail.com>
2016-05-12 16:07:19 +0800

    first commit

---
+++ app/src/main/java/com/trx/emojispaceime/MyKeyboard.java
@@ -0,0 +1,13 @@
+package com.trx.emojispaceime;
+
+import android.content.Context;
+import android.inputmethodservice.Keyboard;
+
+/**
+ * Created by TRX on 05/11/2016.
+ */
+public class MyKeyboard extends Keyboard {
+    public MyKeyboard(Context context, int xmlLayoutResId) {
+        super(context, xmlLayoutResId);
+    }
+}
---
+++ app/src/main/java/com/trx/emojispaceime/SettingsActivity.java
@@ -0,0 +1,10 @@
+package com.trx.emojispaceime;
+
+import android.preference.PreferenceActivity;
+
+/**
+ * Created by TRX on 05/10/2016.
+ */
+public class SettingsActivity extends PreferenceActivity {
+
+}
