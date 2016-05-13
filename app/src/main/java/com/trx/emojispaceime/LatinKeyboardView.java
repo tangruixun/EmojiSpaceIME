f600b2e3fcb809b05c89d536d2428e5c5225f969
tangruixun <tangruixun@gmail.com>
2016-05-13 15:22:07 +0800

    add code from keyboard sample

---
+++ app/src/main/java/com/trx/emojispaceime/LatinKeyboardView.java
@@ -0,0 +1,41 @@
+package com.trx.emojispaceime;
+
+import android.content.Context;
+import android.inputmethodservice.Keyboard;
+import android.inputmethodservice.KeyboardView;
+import android.util.AttributeSet;
+import android.view.inputmethod.InputMethodSubtype;
+
+/**
+ * Created by TRX on 05/12/2016.
+ */
+public class LatinKeyboardView extends KeyboardView {
+
+    static final int KEYCODE_OPTIONS = -100;
+    // TODO: Move this into android.inputmethodservice.Keyboard
+    static final int KEYCODE_LANGUAGE_SWITCH = -101;
+
+    public LatinKeyboardView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public LatinKeyboardView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    @Override
+    protected boolean onLongPress(Keyboard.Key key) {
+        if (key.codes[0] == Keyboard.KEYCODE_CANCEL) {
+            getOnKeyboardActionListener().onKey(KEYCODE_OPTIONS, null);
+            return true;
+        } else {
+            return super.onLongPress(key);
+        }
+    }
+
+    void setSubtypeOnSpaceKey(final InputMethodSubtype subtype) {
+        final LatinKeyboard keyboard = (LatinKeyboard)getKeyboard();
+        keyboard.setSpaceIcon(getResources().getDrawable(subtype.getIconResId()));
+        invalidateAllKeys();
+    }
+}
